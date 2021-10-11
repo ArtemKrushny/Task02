f02bf882dc056b06f2b53d941a12b083ee0574ce
ArtemKrushny <91435075+ArtemKrushny@users.noreply.github.com>
2021-10-12 01:48:01 +0300

    Add files via upload

---
+++ JavaWeb_Epam_Task02_Junit/src/by/company/homework/task02/Task02_1.java
@@ -0,0 +1,62 @@
+package by.company.homework.task02;
+
+import org.junit.*;
+import java.util.Random;
+import static org.junit.Assert.*;
+
+public class Task02_1 {
+    private int[] array;
+
+    public Task02_1( )
+    {
+
+    }
+
+    @Before
+    public void Before( )
+    {
+        array = new int[5];
+        setArray();
+    }
+
+    private void setArray() {
+        Random random = new Random();
+        for (int i=0; i<this.array.length; i++){
+            this.array[i] = random.nextInt(10);
+        }
+    }
+
+    public void deleteSecondElement() {
+        for (int i = 0; i < this.array.length; i += 1) {
+                if ( i % 2 == 1 )
+                {
+                    this.array[i] = 0;
+                }
+        }
+    }
+
+    public String getPrint() {
+        String buff = "";
+        for (int i = 0; i < this.array.length; i++) {
+            buff += " " + this.array[i] ;
+        }
+        return buff ;
+    }
+
+    @Test
+    public void test() {
+        deleteSecondElement();
+        boolean isValid = true ;
+//        array[ 1 ] = -1 ; // специально установить ошибку, для проверки
+        for (int i = 0; i < array.length; i++) {
+            if ( i % 2 == 1 )
+            {
+                if  ( array[ i ] != 0 )
+                {
+                    isValid = false ;
+                }
+            }
+        }
+        assertTrue( this.getClass().getName( ) + " тест провален ", isValid );
+    }
+}
